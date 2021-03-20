fdcf106e0e194db362ed35b7a6cc19eecda556d4
Merchanntt <dolfoeduardo2@gmail.com>
2021-03-19 21:25:13 -0300

    Tasks and Tests - 100%

---
+++ ignite-template-componentizando-a-aplicacao/src/components/Content.tsx
@@ -0,0 +1,19 @@
+interface ContentProps {
+  title: string
+}
+
+export const Content: React.FC<ContentProps> = ({title, children}) => {
+  return (
+        <div className="container">
+            <header>
+              <span className="category">Categoria: <span>{title}</span></span>
+            </header>
+
+            <main>
+              <div className="movies-list">
+                {children}
+              </div>
+            </main>
+          </div>
+  )
+}
---
+++ ignite-template-componentizando-a-aplicacao/src/components/SideBar.tsx
@@ -0,0 +1,12 @@
+export const SideBar:React.FC = ({children}) => {
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {children}
+        </div>
+
+      </nav>
+  )
+}
